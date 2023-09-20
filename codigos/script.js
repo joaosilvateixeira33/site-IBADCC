967146be259754a2b1dbd1e75c51fdf8670c50cc
joaosilvateixeira33 <joaoteixeirasilva33@gmail.com>
2023-09-20 17:08:06 -0300

    update-20-09-23

--- codigos/script.js
+++ codigos/script.js
@@ -1,3 +1,4 @@
+// Efeito de  efeito digitando
 document.addEventListener('DOMContentLoaded', () =>{
     new TypeIt(".animacao",{
         speed:200,
@@ -7,3 +8,26 @@ document.addEventListener('DOMContentLoaded', () =>{
     .type('Inovamos', {DelayNode: 900})
     .go()
 })
+
+// Mostrar/Ocultar detalhes do curso
+function showContent(page) {
+  const pages = ['inicio', 'basico', 'medio', 'pelno', 'secao-atual'];
+
+  for (const p of pages) {
+    const elements = document.getElementsByClassName(p);
+    for (const element of elements) {
+      if (element.classList.contains('secao-atual')) {
+        element.classList.remove('secao-atual');
+        element.style.display = 'none';
+      }
+    }
+  }
+
+  const selectedElements = document.getElementsByClassName(page);
+  for (const element of selectedElements) {
+    element.classList.add('secao-atual');
+    element.style.display = 'block';
+  }
+}
+
+
